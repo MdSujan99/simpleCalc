cb8f2d862f5359ee02ed1e71ae5829184e2b3896
mdsujan <khans8962@gmail.com>
2021-05-11 21:11:25 +0530

    corrected the display div functioning

--- script.js
+++ script.js
@@ -2,34 +2,34 @@ const expKeys = document.getElementsByClassName("expression-keys")[0]
 const fnKeys = document.getElementsByClassName("fn-keys")[0]
 const expressionText = document.getElementById("expression-text")
 const resultText = document.getElementById("result-text")
-
-
-
+// const buttonClickSound = new Audio()
+// buttonClickSound.src = "mouseClick.mp3"
 
 expKeys.onclick = function(event) {
     // console.log(event)
 	if(event.target.nodeName == 'BUTTON'){
         if(event.target.id != 'equals-key-btn'){
-            if(expressionText.innerText == "expression text:")
+            if(expressionText.innerText == "expression text:"){
                 expressionText.innerText = ""
+            }
             if(event.target.innerText === "X"){
                 expressionText.innerText += "*"
             }
-            else{
+            else if(expressionText.innerText.length <= 26){
                 expressionText.innerText += event.target.innerText
             }
+            else{
+                alert("expression too long")
+            }
         }
-        else    // if the user enters equals button
+        else
         { 
-            // evaluate the expressionText
             try{
-                // console.log(eval(expressionText.innerText))
                 resultText.innerText = eval(expressionText.innerHTML)
             }catch(e){
                 console.log(e)
                 resultText.innerText = e
             }
-            // console.log(eval(expressionText.innerHTML))
         }
 	}
 }
@@ -39,7 +39,7 @@ fnKeys.onclick = function(event) {
 	if(event.target.nodeName == 'BUTTON'){
         if(event.target.id == 'fn-AC'){
             expressionText.innerText = ""
-            resultText.innerText = ""
+            resultText.innerText = "result-text"
         }
         else if(event.target.id == 'fn-del'){
             expressionText.innerText = expressionText.innerText.slice(0,-1)
